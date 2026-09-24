0edf7b35d7138c02f505f80526b20c9de83e2708
raykamuniz6-art <raykamuniz6@gmail.com>
2026-09-24 08:37:45 -0300

    Add files via upload

---
+++ estilo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int contador;
+    printf("digite um contador:  ");
+    scanf("%d", &contador);
+    while (contador >= 1){
+        printf("%d\n", contador);
+        contador--;
+    }
+    printf("Lançamento", contador = 1);
+    
+    return 0;
+}
